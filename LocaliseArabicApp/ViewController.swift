c2d280bc78d2db3d5f8bc9aa33e95b0fefead2f9
Rosy Patel <rosypatel109@hotmail.com>
2021-06-14 21:57:00 +0100

    Initial Commit

---
+++ LocaliseArabicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocaliseArabicApp
+//
+//  Created by Rosy Patel on 14/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
