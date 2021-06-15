ae4bca4493fb588534b3bd4d579e852fd984fbb7
Rosy Patel <rosypatel109@hotmail.com>
2021-06-15 16:30:26 +0100

    added constraints for the labels programatically

--- LocaliseArabicApp/ViewController.swift
+++ LocaliseArabicApp/ViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstLabel: UILabel!
+    
+    @IBOutlet weak var secondLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //firstLabel.textAlignment = .natural
+        NSLayoutConstraint.activate([
+            // make the leading side of label 1 be equal to the leading side of the superview
+            firstLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+
+            // make the leading side of label 2 be equal to the trailing side of label 2 with a padding of 10 between them
+            secondLabel.leadingAnchor.constraint(equalTo: firstLabel.trailingAnchor, constant: 10.0),
+
+            // make the trailing side of label 2 be equal to the trailing side of the superview
+            secondLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
     }
 
 
